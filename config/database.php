b9ecff3f5187a3ca9315b0dfcb73df6957ba0ab0
lopez gaming <bongbongcastro19@gmail.com>
2026-01-19 12:48:36 +0800

    Auto-sync after agent response

--- config/database.php
+++ config/database.php
@@ -1,8 +1,8 @@
 <?php
 define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'filipino_catering');
+define('DB_USER', 'u964669618_Lopez');
+define('DB_PASS', 'Huncho2003_');
+define('DB_NAME', 'u964669618_Catering');
 
 $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
