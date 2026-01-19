6129150ad187ef9afe47fabc89c39a804ae22667
lopez gaming <bongbongcastro19@gmail.com>
2026-01-20 01:34:29 +0800

    SMTP UPDATED

--- config/smtp_config.php
+++ config/smtp_config.php
@@ -1,9 +1,9 @@
 <?php
 define('SMTP_HOST', 'smtp.hostinger.com');
 define('SMTP_PORT', 465);
-define('SMTP_USERNAME', 'pochiecatering@pochiecatering.store');
-define('SMTP_PASSWORD', 'Huncho2003_');
-define('SMTP_FROM_EMAIL', 'pochiecatering@pochiecatering.store');
+define('SMTP_USERNAME', 'pochiecatering@gmail.com');
+define('SMTP_PASSWORD', 'wagyoueuurkzcxzp');
+define('SMTP_FROM_EMAIL', 'pochiecatering@gmail.com');
 define('SMTP_FROM_NAME', 'Pochie Catering Services');
 define('SMTP_SECURE', 'ssl');
 define('SMTP_AUTH', true);
